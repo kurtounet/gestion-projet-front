cfef610ec71d2f0e99b83702fdf5cb9502cd4b35
kurtounet <bossut.anthony@gmail.com>
2025-12-08 20:06:52 +0100

    initial var

--- src/app/features/dashboard/stores/status-store.ts
+++ src/app/features/dashboard/stores/status-store.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { StatusService } from '../services/status.service';
+import { IStatus } from '../models/status.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StatusStore {
-  
+
+  readonly statusService = inject(StatusService);
+  statusesLoading = signal<boolean>(false);
+  statusesLoaded = signal<boolean>(false);
+  statuses = signal<IStatus[]>([]);
+
 }
