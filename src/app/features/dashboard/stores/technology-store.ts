26e312d77713233f146f72d92c286a52790dd068
kurtounet <bossut.anthony@gmail.com>
2025-12-08 20:06:14 +0100

    initial var

--- src/app/features/dashboard/stores/technology-store.ts
+++ src/app/features/dashboard/stores/technology-store.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { ITechnology } from '../models/technology.model';
+import { TechnologyService } from '../services/technology.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TechnologyStore {
-  
+
+  readonly technologyService = inject(TechnologyService);
+  technologyLoading = signal<boolean>(false);
+  technologyLoaded = signal<boolean>(false);
+  technologies = signal<ITechnology[]>([]);
+
 }
