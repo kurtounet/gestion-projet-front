d200919adc5719367bf5dd1696f137b8fb77ced3
kurtounet <bossut.anthony@gmail.com>
2025-12-08 20:14:05 +0100

    getAllPriority

--- src/app/features/dashboard/stores/priority-store.ts
+++ src/app/features/dashboard/stores/priority-store.ts
@@ -13,4 +13,20 @@ export class PriorityStore {
 
   priorities = signal<IPriority[]>([]);
 
+  getAllPriority(): void {
+    this.prioritiesLoading.set(true);
+    this.priorityService.getAllPriority().subscribe( {
+      next: (data) => {
+        if (data) {
+          this.priorities.set(data);
+          this.prioritiesLoaded.set(true);
+          this.prioritiesLoading.set(false);
+        }
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des priorités', err);
+        this.prioritiesLoading.set(false);
+      }
+    });
+  }
 }
