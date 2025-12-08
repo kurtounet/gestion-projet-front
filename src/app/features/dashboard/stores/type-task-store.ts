3fb503219d2f925e9624f9b028287843cc15c82d
kurtounet <bossut.anthony@gmail.com>
2025-12-08 20:12:20 +0100

    initial var

--- src/app/features/dashboard/stores/type-task-store.ts
+++ src/app/features/dashboard/stores/type-task-store.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { ITypeTask } from '../models/type-task.model';
+import { TypeTaskService } from '../services/type-task.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TypeTaskStore {
-  
+
+  readonly typeTaskService = inject(TypeTaskService);
+  typeTasksLoading = signal<boolean>(false);
+  typeTasksLoaded = signal<boolean>(false);
+
+  typeTasks = signal<ITypeTask[]>([]);
+
 }
