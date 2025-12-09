1ed03c1325e366769569aa586392a00928c3d3ed
kurtounet <bossut.anthony@gmail.com>
2025-12-09 13:34:07 +0100

    update

--- src/app/features/dashboard/components/side-bar-sprints/side-bar-card-sprint/side-bar-card-sprint.ts
+++ src/app/features/dashboard/components/side-bar-sprints/side-bar-card-sprint/side-bar-card-sprint.ts
@@ -1,8 +1,10 @@
 import { DatePipe, NgOptimizedImage } from '@angular/common';
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { ISprintInstance } from '@app/features/dashboard/models/sprint-instance.model';
 import { ModalService } from '@app/features/dashboard/services/modal.service';
+import { PriorityStore } from '@app/features/dashboard/stores/priority-store';
 import { ProjectInstantStore } from '@app/features/dashboard/stores/project-instant.store';
+import { StatusStore } from '@app/features/dashboard/stores/status-store';
 
 @Component({
   selector: 'app-side-bar-card-sprint',
@@ -12,10 +14,26 @@ import { ProjectInstantStore } from '@app/features/dashboard/stores/project-inst
 })
 export class SideBarcardSprint {
   projectInstanceStore = inject(ProjectInstantStore);
+  statuses = inject(StatusStore).statuses;
+  priorities = inject(PriorityStore).priorities;
   modalService = inject(ModalService);
   sprint = input<ISprintInstance>();
   isVisible: boolean = false;
   isSelected: boolean = false;
+
+
+  statusName = computed(() => {
+  const status = this.statuses().find((status) => status['@id'] === this.sprint()?.status);
+  return status ? status.label : '';
+ });
+  priorityName = computed(() => {
+  const priority = this.priorities().find((priority) => priority['@id'] === this.sprint()?.priority);
+  return priority ? priority.label : '';
+ });
+  color = computed(() => {
+   return this.sprint()?.color;
+
+ });
 
   editSprint(id: number | undefined) {
     this.modalService.open('Edit sprint', 'edit', 'sprint', id);
