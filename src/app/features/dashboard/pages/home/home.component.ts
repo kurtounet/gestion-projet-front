dff128ec7128c41381974fd91ad67ac6a03e2e8b
kurtounet <bossut.anthony@gmail.com>
2026-02-26 09:14:04 +0100

    feat(dashboard): update home page with modal service and clean template

--- src/app/features/dashboard/pages/home/home.component.ts
+++ src/app/features/dashboard/pages/home/home.component.ts
@@ -1,25 +1,27 @@
 import { Component, inject, signal } from '@angular/core';
-
-import { PanelStatistic } from '../../components/statistic/panel-statistic/panel-statistic';
-import { CardProject } from '../../components/card-project/card-project';
-
 import { AsyncPipe } from '@angular/common';
+import { ModalService } from '../../services/modal.service';
+import { CardProject } from '../../components/card-project/card-project';
 import { IProjectInstance } from '../../models/project-instance.model';
-
- 
 import { ProjectInstanceStore } from '../../stores/project-instance.store';
+import { PanelStatistic } from '../../components/statistic/panel-statistic/panel-statistic';
 
 @Component({
   selector: 'app-home',
-  imports: [PanelStatistic, CardProject ],
+  imports: [PanelStatistic, CardProject],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   projects = signal<IProjectInstance[]>([]);
+  modalService = inject(ModalService);
   ProjectInstanceStore = inject(ProjectInstanceStore);
 
   ngOnInit() {
     this.projects = this.ProjectInstanceStore.projects;
+  }
+
+  newProject() {
+    this.modalService.open('Nouveau projet', 'create', 'project');
   }
 }
