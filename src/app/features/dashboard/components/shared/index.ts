248d3657a1ad2d222861c59d0e3c1d1f9f241f44
kurtounet <bossut.anthony@gmail.com>
2026-02-26 13:53:11 +0100

    barrel files

---
+++ src/app/features/dashboard/components/shared/index.ts
@@ -0,0 +1,5 @@
+export * from './form-input/form-input.component';
+export * from './form-select/form-select.component';
+export * from './form-textarea/form-textarea.component';
+export * from './form-file-picker/form-file-picker.component';
+export * from './form-color-picker/form-color-picker.component';
