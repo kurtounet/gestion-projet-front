d93650f017be8222f4f7e67d27aaf0a776d3a605
kurtounet <bossut.anthony@gmail.com>
2026-02-26 09:12:57 +0100

    refactor(dashboard): update sprint-instance-form with custom inputs and store-based labeling

--- src/app/features/dashboard/components/forms/sprint-instance/sprint-instance-form.component.ts
+++ src/app/features/dashboard/components/forms/sprint-instance/sprint-instance-form.component.ts
@@ -9,19 +9,18 @@ import {
 import { SprintInstanceStore } from '@app/features/dashboard/stores/sprint-instance.store';
 import { FormInputComponent } from '../../shared/form-input/form-input.component';
 import { FormSelectComponent } from '../../shared/form-select/form-select.component';
- 
+
 import { FormTextareaComponent } from '../../shared/form-textarea/form-textarea.component';
 import { FormColorPickerComponent } from '../../shared/form-color-picker/form-color-picker.component';
- 
+
 import { PriorityStore } from '@app/features/dashboard/stores/priority.store';
 import { StatusStore } from '@app/features/dashboard/stores/status.store';
- 
+
 import { ISprintInstance } from '@app/features/dashboard/models/sprint-instance.model';
 import { SprintTemplateStore } from '@app/features/dashboard/stores/sprint-template.store';
 import { ProjectInstanceStore } from '@app/features/dashboard/stores/project-instance.store';
 import { IsoDatePipe } from '@app/pipes/IsoDatePipe/iso-date.pipe';
-import { GetPriorityPipe } from '@app/pipes/get-priority-pipe';
-import { GetStatusPipe } from '@app/pipes/get-status-pipe';
+ 
 
 @Component({
   selector: 'app-sprint-instance-form',
@@ -29,71 +28,66 @@ import { GetStatusPipe } from '@app/pipes/get-status-pipe';
   imports: [
     ReactiveFormsModule,
     FormInputComponent,
-    FormSelectComponent,  
+    FormSelectComponent,
     FormTextareaComponent,
     FormColorPickerComponent,
-    
   ],
   templateUrl: './sprint-instance-form.component.html',
   styleUrl: './sprint-instance-form.component.css',
-  providers: [IsoDatePipe, GetPriorityPipe, GetStatusPipe]
+  providers: [IsoDatePipe],
 })
 export class SprintInstanceFormComponent {
   private fb = inject(FormBuilder);
   private isoDatePipe = inject(IsoDatePipe);
-  private getPriority = inject(GetPriorityPipe);
-  private getStatus = inject(GetStatusPipe);
-   // Injection des stores
-   private statusStore = inject(StatusStore);
-   private priorityStore = inject(PriorityStore);
-   private projectInstanceStore = inject(ProjectInstanceStore);
-   private sprintInstanceStore = inject(SprintInstanceStore);
+ 
+  // Injection des stores
+  private statusStore = inject(StatusStore);
+  private priorityStore = inject(PriorityStore);
+  private projectInstanceStore = inject(ProjectInstanceStore);
+  private sprintInstanceStore = inject(SprintInstanceStore);
   private sprintTemplateStore = inject(SprintTemplateStore);
-  
+
   protected statusOptions = this.statusStore.statuses;
   protected priorityOptions = this.priorityStore.priorities;
   protected sprintTemplateOptions = this.sprintTemplateStore.sprintTemplates;
 
- 
   isNew = signal<boolean>(false);
   submitted = signal(false);
 
-
- 
-protected form = this.fb.nonNullable.group({
-      id: [0],
-      projectInstance: [''],
-      priority: [''],
-      sprintTemplate: [''],
-      sprintDependency: [''],
-      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(255)]],
-      description: [''],
-      color: [''],
-      icon: [''],
-      startDate: [new Date().toISOString().substring(0, 10), Validators.required],
-      endDate: [new Date().toISOString().substring(0, 10), Validators.required],
-      status: [''],
-      position: [0],
-      comment: [''],
-      // createdAt: [''],
-      // updatedAt: [''],
-    });
+  protected form = this.fb.nonNullable.group({
+    id: [0],
+    projectInstance: [''],
+    priority: [''],
+    sprintTemplate: [''],
+    sprintDependency: [''],
+    name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(255)]],
+    description: [''],
+    color: [''],
+    icon: [''],
+    startDate: [new Date().toISOString().substring(0, 10), Validators.required],
+    endDate: [new Date().toISOString().substring(0, 10), Validators.required],
+    status: [''],
+    position: [0],
+    comment: [''],
+    // createdAt: [''],
+    // updatedAt: [''],
+  });
   ngOnInit() {
-     const data = this.projectInstanceStore.selectedSprint();
-     console.log(data); 
+    const data = this.projectInstanceStore.selectedSprint();
+   
 
-    if (data && !this.isNew()) {      
+    if (data && !this.isNew()) {
       const formattedData = {
         ...data,
         id: data.id,
-        status:  this.getStatus.transform(data.status),
-        priority: this.getPriority.transform(data.priority),
+         status: this.statusStore.getLabelById(Number(data.status)),
+        priority: this.priorityStore.getLabelById(Number(data.priority)) ,
         startDate: this.isoDatePipe.transform(data.startDate),
-        endDate: this.isoDatePipe.transform(data.endDate)
+        endDate: this.isoDatePipe.transform(data.endDate),
       };
-      
+
       this.form.patchValue(formattedData as any);
-    }     
+    }
   }
 
   get getForm() {
@@ -110,15 +104,21 @@ protected form = this.fb.nonNullable.group({
     const rawValue = this.form.getRawValue();
     const formValue: ISprintInstance = {
       ...rawValue,
+      priority: this.priorityStore.getIdByLabel(rawValue.priority),
+      status: this.statusStore.getIdByLabel(rawValue.status),
       startDate: new Date(rawValue.startDate),
       endDate: new Date(rawValue.endDate),
     };
 
     if (formValue.id === 0 || formValue.id === null) {
-       this.sprintInstanceStore.createSprintInstance(formValue);
-       this.form.reset();
+      this.sprintInstanceStore.createSprintInstance(formValue);
+      this.form.reset();
     } else {
-      this.sprintInstanceStore.updateSprintInstance(String(formValue.id), formValue);
+      const updatedSprintInstance = this.sprintInstanceStore.updateSprintInstance(
+        String(formValue.id),
+        formValue,
+      );
+      console.log(updatedSprintInstance);
     }
   }
 }
